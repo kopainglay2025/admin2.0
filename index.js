877fc2062b5d0d3de68c6b49f13540b20438605d
kopainglay2025 <100895177+kopainglay2025@users.noreply.github.com>
2025-12-01 13:27:15 +0630

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const admin = require('firebase-admin');
 const auth = require('basic-auth');
-const path = require 'path');
+const path = require 'path'); // 🛑 WRONG SYNTAX
 
 // --- 1. Firebase Initialization (DB Setup) ---
 try {
