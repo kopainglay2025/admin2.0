2b0358f8b1e025f53396102e3a2734853d4c70ed
kopainglay2025 <100895177+kopainglay2025@users.noreply.github.com>
2025-12-16 02:56:30 +0630

    Update firestore_admin.py

--- firestore_admin.py
+++ firestore_admin.py
@@ -1,15 +1,13 @@
 # firestore_admin.py
 import firebase_admin
 from firebase_admin import credentials, firestore
-from config import FIREBASE_SERVICE_ACCOUNT_KEY
 
-# Initialize credentials directly from dict
-cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_KEY)
+# Initialize Firebase app
+cred = credentials.Certificate("serviceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
 
-# Initialize Firebase app only once
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+# Get Firestore client
+firestore_db = firestore.client(app)
 
-# Firestore client
-firestore_db = firestore.client()
-firestore_ref = firestore  # For SERVER_TIMESTAMP
+
+print("Firestore connected successfully!")
